16a37b401fa26ea212f92b6ffe60edb3bfab7624
wuthinan123 <wuthinan112233@gmail.com>
2023-01-05 10:56:40 +0700

    Kaypay

---
+++ app/src/main/java/com/example/kaypay/firebase/MessagingService.java
@@ -0,0 +1,20 @@
+package com.example.kaypay.firebase;
+
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MessagingService extends FirebaseMessagingService {
+
+    @Override
+    public void onNewToken(@NonNull String token) {
+        super.onNewToken(token);
+    }
+
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remotemessage) {
+        super.onMessageReceived(remotemessage);
+    }
+}
